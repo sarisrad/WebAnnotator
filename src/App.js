87b0960e99dadcac678061597e36a0129accb000
Sahar <sahare92@gmail.com>
2016-12-21 16:48:52 +0200

    state-example

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      counter: 1
+    }
+  }
+
+  onClick(){
+    this.state.counter ++;
+    this.forceUpdate();
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" onClick={this.onClick.bind(this)}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Counter = {this.state.counter}</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          my Website <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );

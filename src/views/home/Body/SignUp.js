57a8329d953ea08a0474398f2db20d50ae3e77fe
Sahar <sahare92@gmail.com>
2017-01-10 18:53:47 +0200

    nothing much - login - with errors!

--- src/views/home/Body/SignUp.js
+++ src/views/home/Body/SignUp.js
@@ -6,11 +6,12 @@ import FlatButton from 'material-ui/FlatButton';
 
 export default class SignUp extends Component {
 
+
     constructor() {
         super();
         this.state = {
+            server_address: "http://127.0.0.1:8000/register_user/"
         }
-        var server_address = "http://127.0.0.1:8000/register_user/";
     }
 
     reqListener () {
@@ -19,12 +20,12 @@ export default class SignUp extends Component {
 
     registerUser(){
         //Getting the values of the inputs from the boxes and insert it into 
-        var first_name_obj = document.getElementById("first_name_box");
-        var last_name_obj = document.getElementById("last_name_box");
-        var email_obj = document.getElementById("email_box");
-        var password_obj = document.getElementById("password_box");
-        var password_confirm_obj = document.getElementById("password_confirm_box");
-        var affiliation_obj = document.getElementById("affiliation_box");
+        var first_name_obj = document.getElementById("signup_first_name_box");
+        var last_name_obj = document.getElementById("signup_last_name_box");
+        var email_obj = document.getElementById("signup_email_box");
+        var password_obj = document.getElementById("signup_password_box");
+        var password_confirm_obj = document.getElementById("signup_password_confirm_box");
+        var affiliation_obj = document.getElementById("signup_affiliation_box");
         
         //The actual insertion into the dictionary in order to send it as HTTPRequest (JSON format)
         var user_reg_info_dict = {};
@@ -34,10 +35,12 @@ export default class SignUp extends Component {
         user_reg_info_dict["password"] = password_obj.value;
         user_reg_info_dict["affiliation"] = affiliation_obj.value;
 
+        window.alert(JSON.stringify(user_reg_info_dict));
+
         //Sending the registered user info to the server
         var oReq = new XMLHttpRequest();
         oReq.addEventListener("load", this.reqListener);
-        oReq.open("POST", this.server_address);
+        oReq.open("POST", this.state.server_address);
         oReq.send(JSON.stringify(user_reg_info_dict));
 
         return false;        
@@ -49,12 +52,12 @@ export default class SignUp extends Component {
             <div className="SignUp">
                 <h1>Sign Up</h1>
                 <h2>It's free and anyone can join us</h2>
-                 <TextField id="first_name_box" hintText="User Name" onChange={(event) => {console.log(event.target.value)}}/>
-                 <TextField id="last_name_box" hintText="Last Name" />
-                 <TextField id="email_box" hintText="Email" />
-                 <TextField id="password_box" hintText="Password" />
-                 <TextField id="password_confirm_box" hintText="Password Confirmation" />
-                 <TextField id="affiliation_box" hintText="Affiliation" />
+                 <TextField id="signup_first_name_box" hintText="User Name" onChange={(event) => {console.log(event.target.value)}}/>
+                 <TextField id="signup_last_name_box" hintText="Last Name" />
+                 <TextField id="signup_email_box" hintText="Email" />
+                 <TextField id="signup_password_box" hintText="Password" />
+                 <TextField id="signup_password_confirm_box" hintText="Password Confirmation" />
+                 <TextField id="signup_affiliation_box" hintText="Affiliation" />
                  <br></br>
                 <FlatButton onClick={() => this.registerUser()}>Register</FlatButton>
             </div>
--- src/views/home/Header/Login.js
+++ src/views/home/Header/Login.js
@@ -10,28 +10,39 @@ export default class Login extends Component {
     constructor() {
         super();
         this.state = {
+            server_address:"http://127.0.0.1:8000/login_user/",
+            logged_in: false,
+            user_email: ""
         }
-        var server_address = "http://127.0.0.1:8000/register_user/";
     }
 
+
+    // !!!WARNING!!!  Needs to be corrected Because this function is not in "this" scope
     reqListener () {
         console.log(this.responseText);
+        if(this.responseText !== "None"){
+            this.state.user_email = this.responseText;
+            this.state.logged_in = true;
+        }
+        console.log(this.state.user_email);
     }
 
     loginUser(){
         //Getting the values of the inputs from the boxes and insert it into 
-        var email_obj = document.getElementById("email_box");
-        var password_obj = document.getElementById("password_box");
+        var email_obj = document.getElementById("login_email_box");
+        var password_obj = document.getElementById("login_password_box");
         
         //The actual insertion into the dictionary in order to send it as HTTPRequest (JSON format)
         var user_login_info_dict = {};
         user_login_info_dict["email"] = email_obj.value;
         user_login_info_dict["password"] = password_obj.value;
 
+        window.alert(JSON.stringify(user_login_info_dict))
+
         //Sending the registered user info to the server
         var oReq = new XMLHttpRequest();
         oReq.addEventListener("load", this.reqListener);
-        oReq.open("POST", this.server_address);
+        oReq.open("POST", this.state.server_address);
         oReq.send(JSON.stringify(user_login_info_dict));
 
         return false;        
@@ -41,8 +52,8 @@ export default class Login extends Component {
     render() {
         return (
             <div className="Login">
-                 <TextField id="email_box" hintText="Email" />
-                 <TextField id="password_box" hintText="Password" />
+                 <TextField id="login_email_box" hintText="Email" />
+                 <TextField id="login_password_box" hintText="Password" />
                  <FlatButton onClick={() => this.loginUser()}>Login</FlatButton>
             </div>
         )
